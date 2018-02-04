27e9c9063a1ed90998d86e9ad485f1be91b66a28
Fabricio Mendonca Rodrigues <fabriciomendonca@gmail.com>
2018-02-04 21:56:03 +0100

    Initial configuration

---
+++ showroom/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <h1>Business rules package</h1>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ showroom/src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <section>
+      <h2>Users exposer</h2>
+    </section>
+    <section>
+      <h2>Posts exposer</h2>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="css" scoped>
+
+</style>
+
