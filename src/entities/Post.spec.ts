38dd7bea51936f932442eb819cc55854f423ced3
Fabricio Mendonca Rodrigues <fabriciomendonca@gmail.com>
2018-03-04 22:02:10 +0100

    create and test Post entity

---
+++ src/entities/Post.spec.ts
@@ -0,0 +1,135 @@
+import { Post, IPost } from './Post';
+
+describe('Test Post entity', () => {
+  /* tslint:disable-next-line:max-line-length */
+  const bigString = 'est rerum tempore vitae sequi sint nihil reprehenderit dolor beatae ea dolores neque fugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis qui aperiam non debitis possimus qui neque nisi nulla est rerum tempore vitae sequi sint nihil reprehenderit dolor beatae ea dolores neque fugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis qui aperiam non debitis possimus qui neque nisi nulla';
+  let post: IPost;
+
+  beforeEach(() => {
+    post = new Post();
+  });
+
+  it('should return title is invalid', () => {
+    expect(post.isValidTitle()).toBeFalsy();
+  });
+
+  it('should return title is invalid using additional validator', () => {
+    post.title = 'New';
+    expect(post.isValidTitle((title: string): boolean => {
+      return title.length > 3;
+    })).toBeFalsy();
+  });
+
+  it('should return title is invalid for long titles', () => {
+    post.title = bigString;
+    expect(post.isValidTitle()).toBeFalsy();
+  });
+
+  it('should returns title is valid', () => {
+    post.title = 'New post';
+    expect(post.isValidTitle()).toBeTruthy();
+  });
+
+  it('should return title is valid using additional validation', () => {
+    post.title = 'Lorem ipsum';
+    expect(post.isValidTitle((title: string) => {
+      return title.indexOf('dolor') < 0;
+    })).toBeTruthy();
+  });
+
+  it('should return body is invalid', () => {
+    expect(post.isValidBody()).toBeFalsy();
+  });
+
+  it('should return body is invalid using additional validation', () => {
+    post.body = 'Lorem ipsum dolor sit amet';
+    expect(post.isValidBody((body: string): boolean => {
+      return body.length > 30;
+    })).toBeFalsy();
+  });
+
+  it('should return body is valid', () => {
+    post.body = 'Lorem ipsum dolor sit amet';
+    expect(post.isValidBody()).toBeTruthy();
+  });
+
+  it('should return body is valid using additional validation', () => {
+    post.body = 'Lorem ipsum sit amet';
+    expect(post.isValidBody((body: string): boolean => {
+      return body.indexOf('dolor') < 0;
+    })).toBeTruthy();
+  });
+
+  it('should return post is invalid without previous validation', () => {
+    expect(post.isValid()).toBeFalsy();
+  });
+
+  it('should return post is valid without previous validation', () => {
+    post.title = 'Lorem ipsum dolor sit amet';
+    post.body = bigString;
+
+    expect(post.isValid()).toBeTruthy();
+  });
+
+  it('should return post is invalid with previous title validation', () => {
+    post.title = 'Lorem ipsum dolor';
+    post.body = bigString;
+
+    expect(post.isValidTitle((title: string): boolean => {
+      return title.indexOf('dolor') < 0;
+    })).toBeFalsy();
+
+    expect(post.isValid()).toBeFalsy();
+  });
+
+  it('should return post is invalid with previous title and body validation, title is valid', () => {
+    post.title = 'Lorem ipsum dolor';
+    post.body = bigString;
+
+    expect(post.isValidTitle()).toBeTruthy();
+    expect(post.isValidBody((body: string): boolean => {
+      return body.length < 300;
+    })).toBeFalsy();
+
+    expect(post.isValid()).toBeFalsy();
+  });
+
+  it('should return post is invalid with previous title and body validation, body is valid', () => {
+    post.title = 'Lorem ipsum dolor';
+    post.body = bigString;
+
+    expect(post.isValidTitle((title: string): boolean => {
+      return title.indexOf('dolor') < 0;
+    })).toBeFalsy();
+    expect(post.isValidBody()).toBeTruthy();
+
+    expect(post.isValid()).toBeFalsy();
+  });
+
+  it('should return post is valid with previous title validation', () => {
+    post.title = 'Lorem ipsum dolor';
+    post.body = bigString;
+
+    expect(post.isValidTitle()).toBeTruthy();
+    expect(post.isValid()).toBeTruthy();
+  });
+
+  it('should return post is valid with previous body validation', () => {
+    post.title = 'Lorem ipsum dolor';
+    post.body = bigString;
+
+    expect(post.isValidBody()).toBeTruthy();
+    expect(post.isValid()).toBeTruthy();
+  });
+
+  it('should return post is valid with previous title and body validation', () => {
+    post.title = 'Lorem ipsum';
+    post.body = bigString;
+
+    expect(post.isValidTitle((title: string): boolean => {
+      return title.indexOf('dolor') < 0;
+    })).toBeTruthy();
+    expect(post.isValidBody()).toBeTruthy();
+    expect(post.isValid()).toBeTruthy();
+  });
+});
---
+++ src/entities/Post.ts
@@ -0,0 +1,97 @@
+export interface IPost {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+  isValidTitle: (additionalValidator?: (value: string) => boolean) => boolean;
+  isValidBody: (additionalValidator?: (value: string) => boolean) => boolean;
+  isValid: () => boolean;
+}
+
+export class Post implements IPost {
+  public userId: number = 0;
+  public id: number = 0;
+  public title: string = '';
+  public body: string = '';
+
+  /**
+   * Private properties to store validation states
+   * when the application validates fields separetely
+   * and/or use additional validations
+   */
+  private _validTitle: boolean | undefined;
+  private _validBody: boolean | undefined;
+
+  /**
+   * Returns if title property is valid based on the internal validator
+   * and an optional extra validator
+   * @memberof Post
+   * @param validator Additional validation function
+   * @returns boolean
+   */
+  public isValidTitle(validator?: (value: string) => boolean): boolean {
+    this._validTitle = this._validateTitle() && (!validator ? true : validator(this.title));
+    return this._validTitle;
+  }
+
+  /**
+   * Returns if body property is valid based on the internal validator
+   * and an optional extra validator
+   * @memberof Post
+   * @param validator Additional validation function
+   * @returns boolean
+   */
+  public isValidBody(validator?: (value: string) => boolean): boolean {
+    this._validBody = this._validateBody() && (!validator ? true : validator(this.body));;
+    return this._validBody;
+  }
+
+  public isValid(): boolean {
+    if (
+      (
+        this._validTitle &&
+        this._validBody
+      ) ||
+      (
+        this._validTitle &&
+        this._validBody === undefined &&
+        this._validateBody()
+      ) ||
+      (
+        this._validTitle === undefined &&
+        this._validateTitle() &&
+        this._validBody
+      ) ||
+      (
+        this._validTitle === undefined &&
+        this._validBody === undefined &&
+        this._validateTitle() &&
+        this._validateBody()
+      )
+    ) {
+      return true;
+    }
+
+    return false;
+  }
+
+  /**
+   * Validates title property
+   * It should be not empty and should not have more than 256 characters
+   * @memberof Post
+   * @returns boolean
+   */
+  private _validateTitle(): boolean {
+    return this.title.trim() !== '' && this.title.trim().length < 256;
+  }
+
+  /**
+   * Validates body property
+   * It should be not empty and should not have less than 10 characters
+   * @memberof Post
+   * @returns boolean
+   */
+  private _validateBody(): boolean {
+    return this.body.trim() !== '' && this.body.trim().length > 10;
+  }
+}
