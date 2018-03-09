6d91708d37e77824d3dd510f1f3f225154fda1dc
Fabricio Rodrigues <fabricio.rodrigues@daimler.com>
2018-03-09 13:12:49 +0100

    add postExposer.posts to showroom

--- showroom/src/views/Home.vue
+++ showroom/src/views/Home.vue
@@ -2,6 +2,25 @@
   <div class="home">
     <section>
       <h2>Posts exposer</h2>
+      <h3>postExposer.posts</h3>
+      <p>Get all posts from jsonplaceholder.</p>
+      <pre>
+data() {
+  return {
+    posts: [],
+  },
+},
+
+async created() {
+  this.posts = await postExposer.posts;
+},
+      </pre>
+      <small>Just the last 6 posts are shown bellow.</small>
+      <ul v-if="postsSliced.length">
+        <li v-for="post in postsSliced" :key="`post-${post.id}`">
+          <router-link :to="`/posts/${post.id}`">{{post.title}}</router-link>
+        </li>
+      </ul>
     </section>
   </div>
 </template>
@@ -17,6 +36,12 @@ export default {
     };
   },
 
+  computed: {
+    postsSliced() {
+      return this.posts.length > 6 ? this.posts.slice(-6) : this.posts.slice(0).reverse();
+    },
+  },
+
   async created() {
     this.posts = await postExposer.posts;
   },
@@ -24,5 +49,15 @@ export default {
 </script>
 
 <style lang="css" scoped>
+ul {
+  list-style: none;
+}
+pre {
+  background: #ebebeb;
+  margin: 0 auto;
+  max-width: 600px;
+  padding: 10px;
+  text-align: left;
+}
 </style>
 
