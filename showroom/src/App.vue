ea10920385864e96a5cea80fe959e38afd632e72
Fabricio Rodrigues <fabricio.rodrigues@daimler.com>
2018-03-09 16:38:19 +0100

    add form validation and live validation

--- showroom/src/App.vue
+++ showroom/src/App.vue
@@ -16,17 +16,20 @@ export default {
 </script>
 
 <style>
-* {
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
-}
 html {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   font-size: 20px;
   line-height: 1.5;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+}
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+input, textarea {
+  font-size: 1rem;
 }
 .app {
   color: #2c3e50;
@@ -41,7 +44,36 @@ html {
   color: white
 }
 
+.bg-error {
+  background: #e14100;
+}
+
+.bg-success {
+  background: #00b434;
+}
+
+.border-error {
+  border: 2px solid #e14100;
+}
+
 .fg-white {
   color: white;
+}
+
+.tile {
+  box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.25);
+  font-size: 1rem;
+  margin: 10px 0;
+  padding: 10px;
+}
+
+.btn {
+  border: 0;
+  cursor: pointer;
+  opacity: 1;
+}
+.btn[disabled] {
+  cursor: initial;
+  opacity: 0.3;
 }
 </style>
--- showroom/src/views/Home.vue
+++ showroom/src/views/Home.vue
@@ -37,6 +37,53 @@ data() {
 },
 </pre>
       </div>
+      <h3>postsExposer.createPost</h3>
+      <p>Validates the data and creates a new post</p>
+      <h4>Validate on submit</h4>
+      <div class="tile bg-error fg-white" v-if="errorCreate">{{errorCreate}}</div>
+      <div
+        class="tile bg-success fg-white"
+        v-if="successCreate"
+      >
+        Post "{{createdPost.title}}" was created!
+      </div>
+      <form @submit.prevent="createPost" class="form">
+        <label for="title" class="form__label">Title</label>
+        <input id="title" class="form__field" v-model="post.title">
+        <label for="body" class="form__label">Content</label>
+        <textarea id="body" class="form__field" v-model="post.body"></textarea>
+        <button class="form__submit tile btn bg-blue fg-white" type="submit">Create post</button>
+      </form>
+      <h4>Live validation</h4>
+      <div class="tile bg-error fg-white" v-if="errorTitleLive || errorBodyLive">
+        <p v-if="errorTitleLive">{{errorTitleLive}}</p>
+        <p v-if="errorBodyLive">{{errorBodyLive}}</p>
+      </div>
+      <div
+        class="tile bg-success fg-white"
+        v-if="successCreateLive"
+      >
+        Post "{{createdPostLive.title}}" was created!
+      </div>
+      <form @submit.prevent="createPostLive" class="form">
+        <label for="title1" class="form__label">Title</label>
+        <input
+          id="title1"
+          :class="['form__field', {'border-error': errorTitleLive}]"
+          v-model="postLive.title"
+          @input="validateTitleLive"
+        >
+        <label for="body1" class="form__label">Content</label>
+        <textarea
+          id="body1"
+          :class="['form__field', {'border-error': errorBodyLive}]"
+          v-model="postLive.body" @input="validateBodyLive"></textarea>
+        <button
+          class="form__submit tile btn bg-blue fg-white"
+          type="submit"
+          :disabled="!validForm">Create post</button>
+      </form>
+      <h4>Using vuelidate</h4>
     </div>
   </section>
 </template>
@@ -48,14 +95,69 @@ import { postExposer } from 'business-rules-package';
 export default {
   data() {
     return {
+      errorCreate: undefined,
+      successCreate: false,
+      createdPost: undefined,
+      errorTitleLive: undefined,
+      errorBodyLive: undefined,
+      successCreateLive: false,
+      createdPostLive: undefined,
       posts: [],
       post: postExposer.initPost(),
+      postLive: postExposer.initPost(),
+      validForm: false,
     };
   },
 
   computed: {
     postsSliced() {
       return this.posts.length > 6 ? this.posts.slice(-6) : this.posts.slice(0).reverse();
+    },
+  },
+
+  methods: {
+    validateTitleLive() {
+      if (!this.postLive.isValidTitle(title => title.length > 10)) {
+        this.errorTitleLive = 'Title is invalid';
+      } else {
+        this.errorTitleLive = undefined;
+      }
+
+      this.validForm = this.postLive.isValid();
+    },
+
+    validateBodyLive() {
+      if (!this.postLive.isValidBody()) {
+        this.errorBodyLive = 'Content is invalid';
+      } else {
+        this.errorBodyLive = undefined;
+      }
+
+      this.validForm = this.postLive.isValid();
+    },
+
+    async createPost() {
+      try {
+        this.createdPost = await postExposer.createPost(this.post);
+        this.errorCreate = undefined;
+        this.successCreate = true;
+        this.post = postExposer.initPost();
+      } catch (err) {
+        this.errorCreate = err.message;
+        this.successCreate = false;
+      }
+    },
+
+    async createPostLive() {
+      try {
+        this.createdPostLive = await postExposer.createPost(this.postLive);
+        this.errorCreateLive = undefined;
+        this.successCreateLive = true;
+        this.postLive = postExposer.initPost();
+      } catch (err) {
+        this.errorCreateLive = err.message;
+        this.successCreateLive = false;
+      }
     },
   },
 
@@ -76,6 +178,26 @@ ul {
   overflow: auto;
   padding: 10px;
   text-align: left;
+}
+
+.form {
+  display: flex;
+  flex-direction: column;
+  max-width: 600px;
+}
+
+.form__label {
+  margin: 10px 0;
+}
+
+.form__field {
+  margin-bottom: 20px;
+  padding: 5px;
+}
+
+.form__submit {
+  align-self: flex-end;
+  flex-grow: 0;
 }
 </style>
 
